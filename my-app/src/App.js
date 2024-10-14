d1b3da28b7e66c32e4af85575ba5cd6b4d2e5fa9
Slava Homenko <hosl_boy@inbox.ru>
2024-10-14 18:18:22 +0300

    Управляемые компоненты

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 function App() {
+    const [likes,setLikes] = useState(2)
+    const [value,setValue] = useState('Текст в инпуте')
+    
+    function increment(){
+      setLikes(likes + 1)
+    }
+    function decrement(){
+      setLikes(likes - 1)
+    }
   return (
     <div className="App">
-      
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input 
+        type="text" 
+        value={value} 
+        onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={increment} >increment</button>
+      <button onClick={decrement} >decrement</button>
     </div>
   );
 }
