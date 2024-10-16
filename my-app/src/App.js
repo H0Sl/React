b3590fd10a75ad93ed3977d0d6b553286d3372c5
Slava Homenko <hosl_boy@inbox.ru>
2024-10-16 19:55:55 +0300

    Статичный список

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import './style/app.css';
+import PostList from './components/PostList';
 
 
 function App() {
-    const [value,setValue] = useState('Текст в инпуте')
+  const [posts,serPosts] = useState([
+    {id:1,title:'JavaScript',body:'Description'},
+    {id:2,title:'JavaScript 2',body:'Description'},
+    {id:3,title:'JavaScript 3',body:'Description'},
+  ])
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <PostList posts={posts} title={'List Posts 1'}/>
     </div>
   );
 }
---
+++ my-app/src/components/postItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PostItem = function(props){
+    console.log(props);
+
+
+    return(
+        <div className='post'>
+        <div className='post__contant'>
+          <strong>{props.post.id}. {props.post.title}</strong>
+          <div>
+              {props.post.body}.
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Delete</button>
+        </div>
+      </div>
+    );
+};
+
+export default PostItem;
