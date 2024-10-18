99462eac23b9eb497bad317f392d377f8ed62488
Slava Homenko <hosl_boy@inbox.ru>
2024-10-18 13:01:45 +0300

    Сортирровка постов

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from './components/ClassCounter';
 import './style/app.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
     {id:3,title:'JavaScript 3',body:'Description'},
   ]);
 
+  const [selectedSort,setSelectedSort] = useState('')
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -21,9 +24,26 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin:'15px 0'}} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultVelue='Sort'
+          options={[
+            {value:'title',name:'Title'},
+            {value:'body',name:'Text'}
+          ]}
+        />
+      </div>
       {posts.length !== 0
         ? 
         <PostList remove={removePost} posts={posts} title={'List Posts 1'}/>
