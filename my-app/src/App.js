60c6cd83208b7c45aabec48d83b5e89af4ba5f45
Slava Homenko <hosl_boy@inbox.ru>
2024-10-18 21:27:48 +0300

    Компонет Pagination

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,8 @@ import axios from 'axios';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
-import { getPageCount,getPagesArray } from './utils/pages';
+import { getPageCount,getPagesArray} from './utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
   const [posts,setPosts] = useState([]);
@@ -21,9 +22,8 @@ function App() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
-  let pagesArray = getPagesArray(totalPages)
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () =>{
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit,page) =>{
     const response = await PostService.getAll(limit,page);
     setPosts(response.data)
     const totalCount = (response.headers['x-total-count'])
@@ -31,8 +31,8 @@ function App() {
   })
 
   useEffect(() => {
-    fetchPosts()
-  },[page])
+    fetchPosts(limit,page)
+  },[])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -45,6 +45,7 @@ function App() {
 
   const changePage = (page) => {
     setPage(page)
+    fetchPosts(limit,page)
   }
 
   return (
@@ -69,17 +70,11 @@ function App() {
           </div>
         : <PostList remove={removePost} posts={sortedAndSearchPosts} title={'List Posts 1'}/>
       }
-      <div className='page__wrapper'>
-        {pagesArray.map(p => 
-          <span 
-            onClick={() => changePage(p)}
-            key={p} 
-            className={page === p? 'page page__current' : 'page'}
-          >
-            {p}
-          </span>
-        )}
-      </div>
+      <Pagination
+        page={page}
+        changePage={changePage}
+        totalPages={totalPages}
+      />
     </div>
   );
 }
